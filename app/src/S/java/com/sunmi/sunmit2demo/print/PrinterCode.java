b40dbfe9b6a3613f92b561483f18c948665016e6
rc <644046154@qq.com>
2019-05-26 19:11:52 +0800

    1.接入 打印机 相关库及 打印机的连接和打印功能

---
+++ app/src/S/java/com/sunmi/sunmit2demo/print/PrinterCode.java
@@ -0,0 +1,14 @@
+package com.sunmi.sunmit2demo.print;
+
+/**
+ * author : chrc
+ * date   : 2019/5/26  7:05 PM
+ * desc   :
+ */
+public class PrinterCode {
+
+    /**
+     * 使用打印机指令错误
+     */
+    public static final int PRINTER_COMMAND_ERROR = 0x008;
+}
