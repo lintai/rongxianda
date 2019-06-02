69824ee3b50419ff278845fd84cd7507dccef8f9
rc <644046154@qq.com>
2019-06-02 22:05:00 +0800

    1.选择支付方式页面 ui调整 2.称重商品加入购物栏 bug 修复

--- app/src/S/java/com/sunmi/sunmit2demo/eventbus/GoodsItemClickEvent.java
+++ app/src/S/java/com/sunmi/sunmit2demo/eventbus/GoodsItemClickEvent.java
@@ -12,18 +12,21 @@ public class GoodsItemClickEvent {
     public String unit;//计量单位，比如38元/300g 中的/300g
     public String goodsCode;//商品编码，称重物品则是plu吗
     public int priceType;//价格类型；1-计件 2-称重
-    public float count;//称重商品专用
+
+    public float weight;//称重商品专用
+    public float totalPrice;//称重商品专用
 
     public GoodsItemClickEvent(String goodsName, int price, String unit, String goodsCode, int priceType) {
-        this(goodsName, price, unit, goodsCode, priceType, 0);
+        this(goodsName, price, unit, goodsCode, priceType, 0, 0);
     }
 
-    public GoodsItemClickEvent(String goodsName, int price, String unit, String goodsCode, int priceType, float count) {
+    public GoodsItemClickEvent(String goodsName, int price, String unit, String goodsCode, int priceType, float weight, float totalPrice) {
         this.goodsName = goodsName;
         this.price = price;
         this.unit = unit;
         this.goodsCode = goodsCode;
         this.priceType = priceType;
-        this.count = count;
+        this.weight = weight;
+        this.totalPrice = totalPrice;
     }
 }
